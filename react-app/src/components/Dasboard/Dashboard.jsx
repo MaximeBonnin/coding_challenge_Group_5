f5e30a16f037ad8fefef89ed471a74e24c60e7d6
Maxime Bonnin <maxime.bonnin@gmx.de>
2023-08-04 15:22:39 +0100

    add basic design

--- react-app/src/components/Dasboard/Dashboard.jsx
+++ react-app/src/components/Dasboard/Dashboard.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { AllBonds } from '../AllBonds/AllBonds';
+import Card from "react-bootstrap/Card";
+import Filter from './Filter';
 
 
 const Dasboard = () => {
   return(
     <>
-      <h2>Dashboard</h2>
-      <AllBonds/>
+    <header>
+      <img id='logo-image' src="https://th.bing.com/th/id/OIP.DsY0EH8WGf2CtXcvTARfQgHaEK?pid=ImgDet&rs=1"  alt=""/>
+    </header>
+    <div className="content-container">
+    <Filter />
+    <Card>   
+      <Card.Body>
+      <Card.Title >Your Bonds</Card.Title> 
+      <Card.Text><AllBonds/></Card.Text>  
+      </Card.Body>     
+    </Card>
+
+    
+      
+      
+    </div>
     </>
   );
 }
