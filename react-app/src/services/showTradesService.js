7d0658ca61d25791b40c12dec31e074485bb8c3b
lukrids <lukasmueller@gmail.com>
2023-08-07 17:30:39 +0100

    delete all consol.log stuff for testing during develoopping

--- react-app/src/services/showTradesService.js
+++ react-app/src/services/showTradesService.js
@@ -5,11 +5,9 @@
 export const fetchTrades = async (bookID, isin) => {
   const tokenString = localStorage.getItem('token');
   const token = JSON.parse(tokenString);
-  console.log(bookID);
 
   const path = "/" + isin;
   const url = 'http://localhost:8080/api/v1/trades' + path;
-  console.log(url);
 
   var data = await fetch(url, {
     method: 'GET',
@@ -21,16 +19,13 @@ export const fetchTrades = async (bookID, isin) => {
     .then((response) => response.json())
     .catch((error) => console.error(error));
 
-  console.log(data);
   if (bookID != null && bookID.length > 0) {
     var return_data = [];
     data.forEach(element => {
-      console.log(element, bookID.includes(element.book_id));
       if (bookID.includes(element.book_id)) {
         return_data.push(element);
       }
     })
-    console.log(return_data);
     return return_data;
   }
 
